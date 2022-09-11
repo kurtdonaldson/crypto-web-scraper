97b10d2f39e3cfb7db86dff041696474e47cb2a2
DONNY345 <89885417+DONNY345@users.noreply.github.com>
2022-09-12 10:55:52 +1200

    second stage

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const cheerio = require("cheerio");
 const express = require("express");
 const axios = require("axios");
 const path = require("path");
+const { setPriority } = require("os");
 const PORT = 3000;
 
 const app = express();
 
 const url = "https://coinmarketcap.com/";
 
-const crytoDataArray = ["number", "name", "price", "change24h"];
+
 
 //Array of objects? Try figure this out
 
 async function getCrytoPrices() {
+  
   //Fetch data using axios
   const { data } = await axios.get(url);
   //Load html in cheerio. We parse it the html data
@@ -20,19 +22,55 @@ async function getCrytoPrices() {
   //Access data
   const elemSelector =
     "#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div > div.h7vnx2-1.bFzXgL > table > tbody > tr";
-  $(elemSelector).each((index, element) => {
-    if (index <= 10) {
-      $(element)
+  //Store results
+  const crytoDataKeys = [
+    "position", 
+    "name", 
+    "price", 
+    "1hr%", 
+    "24h%", 
+    "7day%",
+    "marketCap",
+    "volume24h",
+    "circulatingSupply"
+  ];
+
+  
+
+    $(elemSelector).each((parentIndex, parentElement) => {
+  //We have a key index variable set to 0. This is so we can increment the index
+  // to allow the key-value pairs to be ordered appropriately. 
+    let cryptoKeyIndex = 0;
+    const crypoObject = {}
+    //parent index set to <=9 because we only want top 10 crytpo prices
+    if (parentIndex <= 9) {
+      $(parentElement)
         .children()
         .each((childIndex, childElement) => {
-          const tableCellValue = $(childElement).text();
+          let tableCellValue = $(childElement).text();
+          // below we are tidying up the name, marketcap and 24hr volume.
+          //First, we pass in child to cheerio. We then select the first child
+          //from childElement.html, then we grab the text from this. 
+          if(cryptoKeyIndex === 1 || cryptoKeyIndex === 6 || cryptoKeyIndex === 7){
+           console.log(
+           $('p:first-child', $(childElement).html()).text());
+          }
+
+
+
 
           //   Filter out blank space. Will check if tableCellVale is false
           if (tableCellValue) {
-            console.log(tableCellValue);
+            // console.log(crytoDataArray[cryptoKeyIndex]);
+            crypoObject[crytoDataKeys[cryptoKeyIndex]] = tableCellValue;
+
+            //Every time the above function runs, we will increment by 1
+            cryptoKeyIndex++;
           }
         });
+        // console.log(crypoObject)
     }
+
   });
 }
 
@@ -49,3 +87,6 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
+
+
+// Current setPriority - Filter out marketcap and volume 24 extra text
